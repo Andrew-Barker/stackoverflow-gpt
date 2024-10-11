3581131a6ca42ea49ae23accf2ea385110ba4f25
Andrew Barker <andrew.barker@kroger.com>
2024-10-10 20:16:14 -0400

    mock active tab for now

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,20 +3,24 @@ import { FaHome, FaQuestionCircle, FaTags, FaUsers } from "react-icons/fa";
 import { MdHome } from "react-icons/md";
 import { BiBuildings } from "react-icons/bi";
 
+const activeClasses = "font-bold text-black bg-gray-100 rounded-l-md";
+
 const Navbar = () => {
   return (
     <div className="w-52 bg-white pl-4 pt-6 text-sm fixed top-16 h-auto">
       {/* Navbar Links */}
       <ul className="space-y-2">
-        <li className="flex items-center space-x-3 p-1 hover:bg-gray-100 hover:border hover:border-l hover:rounded-l-md cursor-pointer">
+        <li className="flex items-center space-x-3 p-2 hover:bg-gray-100 hover:border hover:border-l hover:rounded-l-md cursor-pointer">
           <MdHome className="text-black text-xl" />
           <span className="text-black">Home</span>
         </li>
-        <li className="flex items-center space-x-3 p-1 hover:bg-gray-100 hover:border hover:border-l hover:rounded-l-md cursor-pointer">
+        <li
+          className={`flex items-center space-x-3 p-2 hover:bg-gray-100 hover:border hover:border-l hover:rounded-l-md cursor-pointer ${activeClasses}`}
+        >
           <FaQuestionCircle className="text-black text-xl" />
           <span className="text-black">Questions</span>
         </li>
-        <li className="flex items-center space-x-3 p-1 hover:bg-gray-100 hover:border hover:border-l hover:rounded-l-md cursor-pointer">
+        <li className="flex items-center space-x-3 p-2 hover:bg-gray-100 hover:border hover:border-l hover:rounded-l-md cursor-pointer">
           <FaTags className="text-black text-xl" />
           <span className="text-black">Tags</span>
         </li>
